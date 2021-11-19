fdd957cb1d9d6a1363cb8e20681e0a7ead5f8151
Mazi82 <92712976+Mazi82@users.noreply.github.com>
2021-11-20 00:16:01 +0100

    commited Sorttest

--- src/Test/Sorttest.java
+++ src/Test/Sorttest.java
@@ -1,5 +1,5 @@
 package Test;
-import Sorter.Sorter;
+import Sort.Sorter;
 
 public class Sorttest {
     public static void main(String[] args) {
@@ -12,9 +12,48 @@ public class Sorttest {
             System.err.println("Error Test1: Not Sorted");
         }
         if (testSorterMain2()) {
-            System.out.println("Test1 Ok");
+            System.out.println("Test2 Ok");
         } else {
             System.err.println("Error Test2: Not Sorted");
         }
+        if (testSorterMain3()) {
+            System.out.println("Test3 Ok");
+        } else {
+            System.err.println("Error Test3: Not Sorted");
+        }
+        if (testSorterMain4()) {
+            System.out.println("Test4 Ok");
+        } else {
+            System.err.println("Error Test4: Not Sorted");
+        }
+        if (testSorterMain5()) {
+            System.out.println("Test5 Ok");
+        } else {
+            System.err.println("Error Test5: Not Sorted");
+        }
+
+        return true;
     }
+
+
+
+    public static boolean testSorterMain1(){
+        Sorter s = new Sorter();
+        int[] arrList = {5, 225, 555, 185, 375};
+        int[] result = s.sort(arrList);
+        int[] expected = {5, 185, 225, 375, 555};
+        for (int a = 0; a < result.length; a++) {
+            if (result[a] != expected[a]) {
+                System.out.println("Test1 Failed");
+                return false;
+            }
+        }
+    }
+    private static boolean testSorterMain2() {
 }
+    private static boolean testSorterMain3() {
+    }
+    private static boolean testSorterMain4() {
+        }
+    }private static boolean testSorterMain5() {
+        }
